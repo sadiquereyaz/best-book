04497071063c44c349f80eebd6301b4541f6c494
sadiquereyaz <mdsadique47@gmail.com>
2025-03-14 16:39:44 +0530

    auth flow fixed

--- app/src/main/java/com/nabssam/bestbook/presentation/theme/Color.kt
+++ app/src/main/java/com/nabssam/bestbook/presentation/theme/Color.kt
@@ -2,6 +2,90 @@ package com.nabssam.bestbook.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
+val primaryLight = Color(0xFF223876)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFFFFFFF)
+val onPrimaryContainerLight = Color(0xFF11144B)
+val secondaryLight = Color(0xFF5C5D72)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFFFFFFFF)
+val onSecondaryContainerLight = Color(0xFF191A2C)
+val tertiaryLight = Color(0xFF78536A)
+val onTertiaryLight = Color(0xFFFFFFFF)
+val tertiaryContainerLight = Color(0xFFFFFFFF)
+val onTertiaryContainerLight = Color(0xFF2E1126)
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorContainerLight = Color(0xFFFFDAD6)
+val onErrorContainerLight = Color(0xFF410002)
+val backgroundLight = Color(0xFFFFFFFF)
+val onBackgroundLight = Color(0xFF1B1B21)
+val surfaceLight = Color(0xFFFFFFFF)
+val onSurfaceLight = Color(0xFF1B1B21)
+val surfaceVariantLight = Color(0xFFFFFFFF)
+val onSurfaceVariantLight = Color(0xFF46464F)
+val outlineLight = Color(0xFF777680)
+val outlineVariantLight = Color(0xFFC7C5D0)
+val scrimLight = Color(0xFF000000)
+val inverseSurfaceLight = Color(0xFF303036)
+val inverseOnSurfaceLight = Color(0xFFFFFFFF)
+val inversePrimaryLight = Color(0xFFFFFFFF)
+val surfaceDimLight = Color(0xFFFFFFFF)
+val surfaceBrightLight = Color(0xFFFFFFFF)
+val surfaceContainerLowestLight = Color(0xFFFFFFFF)
+val surfaceContainerLowLight = Color(0xFFFFFFFF)
+val surfaceContainerLight = Color(0xFFFFFFFF)
+val surfaceContainerHighLight = Color(0xFFFFFFFF)
+val surfaceContainerHighestLight = Color(0xFFFFFFFF)
+
+val primaryDark = Color(0xFFBFC2FF)
+val onPrimaryDark = Color(0xFF272B60)
+val primaryContainerDark = Color(0xFF3E4278)
+val onPrimaryContainerDark = Color(0xFFE0E0FF)
+val secondaryDark = Color(0xFFC5C4DD)
+val onSecondaryDark = Color(0xFF2E2F42)
+val secondaryContainerDark = Color(0xFF444559)
+val onSecondaryContainerDark = Color(0xFFE1E0F9)
+val tertiaryDark = Color(0xFFE8B9D4)
+val onTertiaryDark = Color(0xFF46263B)
+val tertiaryContainerDark = Color(0xFF5E3C52)
+val onTertiaryContainerDark = Color(0xFFFFD8ED)
+val errorDark = Color(0xFFFFB4AB)
+val onErrorDark = Color(0xFF690005)
+val errorContainerDark = Color(0xFF93000A)
+val onErrorContainerDark = Color(0xFFFFDAD6)
+val backgroundDark = Color(0xFF131318)
+val onBackgroundDark = Color(0xFFE4E1E9)
+val surfaceDark = Color(0xFF131318)
+val onSurfaceDark = Color(0xFFE4E1E9)
+val surfaceVariantDark = Color(0xFF46464F)
+val onSurfaceVariantDark = Color(0xFFC7C5D0)
+val outlineDark = Color(0xFF918F9A)
+val outlineVariantDark = Color(0xFF46464F)
+val scrimDark = Color(0xFF000000)
+val inverseSurfaceDark = Color(0xFFE4E1E9)
+val inverseOnSurfaceDark = Color(0xFF303036)
+val inversePrimaryDark = Color(0xFF565992)
+val surfaceDimDark = Color(0xFF131318)
+val surfaceBrightDark = Color(0xFF39393F)
+val surfaceContainerLowestDark = Color(0xFF0E0E13)
+val surfaceContainerLowDark = Color(0xFF1B1B21)
+val surfaceContainerDark = Color(0xFF1F1F25)
+val surfaceContainerHighDark = Color(0xFF2A292F)
+val surfaceContainerHighestDark = Color(0xFF35343A)
+
+val Purple80 = Color(0xFFD0BCFF)
+val BoxColor = Color(0xfff3f3f3)
+val Star = Color(0xFFFF9800)
+val GreenColor=Color(0xFF4CAF50)
+
+
+
+/*
+package com.nabssam.bestbook.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
 val primaryLight = Color(0xFF565992)
 val onPrimaryLight = Color(0xFFFFFFFF)
 val primaryContainerLight = Color(0xFFE0E0FF)
@@ -81,3 +165,5 @@ val GreenColor=Color(0xFF4CAF50)
 
 
 
+
+*/
--- app/src/main/java/com/nabssam/bestbook/presentation/ui/account/auth/composable/LoginStep.kt
+++ app/src/main/java/com/nabssam/bestbook/presentation/ui/account/auth/composable/LoginStep.kt
@@ -1,19 +1,26 @@
 package com.nabssam.bestbook.presentation.ui.account.auth.composable
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -24,13 +31,21 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.RectangleShape
+
 import com.nabssam.bestbook.R
 import com.nabssam.bestbook.presentation.ui.account.auth.AuthEvent
 import com.nabssam.bestbook.presentation.ui.account.auth.AuthState
@@ -56,8 +71,8 @@ fun LoginStep(
                 imageVector = ImageVector.vectorResource(if (isSystemInDarkTheme()) R.drawable.logo_night else R.drawable.logo),
                 "",
                 modifier = Modifier.fillMaxWidth(0.6f)
-            )
-            Spacer(modifier = Modifier.height(24.dp))
+                    )
+        Spacer(modifier = Modifier.height(24.dp))
             OutlinedTextField(
                 value = state.username,
                 onValueChange = { if (it.length < 10) onEvent(AuthEvent.UpdateUsername(it)) },
@@ -84,7 +99,18 @@ fun LoginStep(
 
         Button(
             onClick = { onEvent(AuthEvent.SignIn) },
-            modifier = Modifier.fillMaxWidth(),
+            //modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(Color(0xFF223876), Color(0xFF1e429f),Color(0xFF5C1ED9)),
+                ),
+                shape = RoundedCornerShape(100)
+                ),
             enabled = validate()
         ) {
             Text("Login")
