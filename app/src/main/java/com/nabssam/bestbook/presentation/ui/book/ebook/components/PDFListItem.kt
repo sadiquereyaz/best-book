c971767ed38f4312ae1867e42f87dfc8bfa6bc7f
sadiquereyaz <mdsadique47@gmail.com>
2025-03-25 13:43:36 +0530

    feat: Add download status indicator and loading state to PDFListItem
    This commit enhances the `PDFListItem` composable to provide visual feedback on the ebook download status and to display a loading indicator during the download process.
    
    The following changes were made:
    
    -   **Download Status Indicator:**
        - Modified `PDFListItem` to display an appropriate icon based on the `isDownloaded` flag of the `Ebook` data class.
        - If the ebook is downloaded, it now shows a "download done" icon; otherwise, it shows a "download" icon.
    -   **Loading State:**
    -      - Added a `isDownloading` state variable to track if the ebook is currently being downloaded.
    -   - Added conditional logic to display a `Loading` composable instead of the download icon when `isDownloading` is true and the ebook is not downloaded yet.
    -  - The `isDownloading` flag is now set to `true` when the user clicks on an ebook item.
    +  - **Loading State:**
    +  - Added a `isDownloading` state variable using `remember` and `mutableStateOf` to track if the ebook is currently being downloaded.
    +   - Added conditional logic to display a `Loading` composable, with a specific size of 24.dp, instead of the download icon when `isDownloading` is true and the ebook is not yet downloaded.
    +    - The `isDownloading` flag is now set to `true` when the user clicks on an ebook item.
    + - **UI Enhancements:**
    +   - Wrapped `getRandomColor` function in `remember` to avoid unnecessary recompositions.
    + - **Code Enhancements:**
    +  - Added modifier to Text to remove extra space, make it visible for border.

--- app/src/main/java/com/nabssam/bestbook/presentation/ui/book/ebook/components/PDFListItem.kt
+++ app/src/main/java/com/nabssam/bestbook/presentation/ui/book/ebook/components/PDFListItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,6 +22,10 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -37,6 +42,7 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.nabssam.bestbook.R
 import com.nabssam.bestbook.presentation.ui.book.ebook.Ebook
+import com.nabssam.bestbook.presentation.ui.components.Loading
 import com.nabssam.bestbook.utils.getRandomColor
 
 @Composable
@@ -45,14 +51,18 @@ fun PDFListItem(
     pdf: Ebook,
     onItemClick: () -> Unit
 ) {
-    val random = getRandomColor()
+    val random = remember { getRandomColor() }
+    var isDownloading by remember { mutableStateOf(false) }
     Card(
         modifier = modifier
             .fillMaxWidth()
             //.padding(vertical = 8.dp, horizontal = 16.dp)
             .border(width = 1.5.dp, color = random, shape = RoundedCornerShape(12.dp))
             .height(dimensionResource(R.dimen.book_height_home))
-            .clickable { onItemClick() },
+            .clickable {
+                onItemClick()
+                isDownloading = true
+            },
         //elevation = CardDefaults.elevatedCardElevation(4.dp),
         colors = CardDefaults.cardColors(
             containerColor = random.copy(alpha = 0.1f),
@@ -103,30 +113,36 @@ fun PDFListItem(
                 Text(
                     modifier = Modifier
                         .align(Alignment.BottomStart)
-                        .offset(x = dimensionResource(R.dimen.book_width_home) + 8.dp, y = -8.dp)
+                        .offset(x = dimensionResource(R.dimen.book_width_home) + 8.dp, y = (-8).dp)
                         .clip(RoundedCornerShape(50))
                         .background(MaterialTheme.colorScheme.background)
-                        .padding(horizontal = 8.dp)
-                    ,
+//                        .border(1.dp, random, RoundedCornerShape(50))
+                        .padding(horizontal = 8.dp),
                     text = it,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    color = MaterialTheme.colorScheme.primary
+//                    color = MaterialTheme.colorScheme.primary
                 )
             }
             IconButton(
                 modifier = Modifier
                     .align(Alignment.BottomEnd),
-                onClick = onItemClick
+                onClick = {
+                    onItemClick()
+                    isDownloading = true
+                }
             ) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(
-                        if (pdf.isDownloaded)
-                            R.drawable.baseline_download_done_24
-                        else R.drawable.download
-                    ),
-                    contentDescription = null
-                )
+                if (isDownloading && !pdf.isDownloaded)
+                    Loading(modifier = Modifier.size(24.dp))
+                else
+                    Icon(
+                        imageVector = ImageVector.vectorResource(
+                            if (pdf.isDownloaded)
+                                R.drawable.baseline_download_done_24
+                            else R.drawable.download
+                        ),
+                        contentDescription = null
+                    )
             }
         }
 
